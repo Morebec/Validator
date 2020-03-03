57f8dffa731afe0ac7d5962ceb60322a3c9ff1e2
Mykhailo Boiev <michaelboyev@gmail.com>
2020-03-03 20:05:59 +0100

    Closes #5 - Add NotEmpty rule (#9)
    * Add Not empty rule for array testing

---
+++ src/Rule/NotEmpty.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Morebec\Validator\Rule;
+
+
+use Morebec\Validator\ValidationRuleInterface;
+
+class NotEmpty implements ValidationRuleInterface
+{
+    /**
+     * @var string|null
+     */
+    private $message;
+
+    public function __construct(
+        ?string $message = null
+    )
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Validates a value according to this rule and returns if it is valid or not
+     * @param mixed $v
+     * @return bool true if valid, otherwise false
+     */
+    public function validate($v): bool
+    {
+        return !empty($v);
+    }
+
+    /**
+     * Returns the message to be used in case the validation did not pass
+     * @param mixed $v the value that did not pass the validation
+     * @return string
+     */
+    public function getMessage($v): string
+    {
+        return $this->message?:"The value of '{$v}' was not expected to be empty";
+    }
+}
---
+++ tests/Rule/NotEmptyTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Morebec\Validator\Rule;
+
+use Morebec\Validator\Rule\NotEmpty;
+use PHPUnit\Framework\TestCase;
+
+class NotEmptyTest extends TestCase
+{
+    public function testValidate()
+    {
+        $rule = new NotEmpty();
+        $this->assertTrue($rule->validate([1,2]));
+        $this->assertFalse($rule->validate([]));
+        $ruleSecond = new NotEmpty("Custom message");
+        $this->assertEquals("Custom message", $ruleSecond->getMessage("arr"));
+        $this->assertEquals("The value of 'arr' was not expected to be empty",$rule->getMessage("arr"));
+    }
+}
