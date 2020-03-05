b54b4ecc28edadb54f999b818f3b620de8153617
Mykhailo Boiev <michaelboyev@gmail.com>
2020-03-05 18:45:46 +0100

    Closes #4 - Add IsUrl rule (#13)
    * Add IsUrl validation rule

---
+++ src/Rule/IsUrl.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Morebec\Validator\Rule;
+
+
+use Morebec\Validator\ValidationRuleInterface;
+
+class isUrl implements ValidationRuleInterface
+{
+    /**
+     * @var string|null
+     */
+    private $message;
+
+    public function __construct(
+        ?string $message = null
+    )
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Validates a value according to this rule and returns if it is valid or not
+     * @param mixed $v
+     * @return bool true if valid, otherwise false
+     */
+    public function validate($v): bool
+    {
+        return filter_var($v, FILTER_VALIDATE_URL);
+    }
+
+    /**
+     * Returns the message to be used in case the validation did not pass
+     * @param mixed $v the value that did not pass the validation
+     * @return string
+     */
+    public function getMessage($v): string
+    {
+        return $this->message?:"'{$v}' was expected to be an URL.";
+    }
+}
