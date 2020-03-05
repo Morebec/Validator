5580609faefacd08199a7f8e4dc268e17e5d9e0b
hellReuter_ <hellreuter@gmail.com>
2020-03-05 06:52:04 -0600

    Add IsIPAddress Rule issue#10

---
+++ tests/Rule/IsIPAddressTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Rule;
+
+
+use Morebec\Validator\Rule\IsIPAddress;
+use PHPUnit\Framework\TestCase;
+
+class IsIPAddressTest extends TestCase
+{
+
+    public function testValidate()
+    {
+        $rule = new IsIPAddress();
+        $this->assertTrue($rule->validate('192.168.1.1'));
+        $this->assertFalse($rule->validate('275.265.11.1'));
+        $this->assertFalse($rule->validate('test'));
+        $this->assertFalse($rule->validate('domain.com'));
+        $this->assertFalse($rule->validate(true));
+        $this->assertFalse($rule->validate(false));
+        $this->assertFalse($rule->validate(1));
+        $this->assertFalse($rule->validate(0));
+        $this->assertFalse($rule->validate([]));
+    }
+}
