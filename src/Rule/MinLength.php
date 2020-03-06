5959b86eb8e73d24563bac6da50f025ef13b07ac
M1QN <michaelboyev@gmail.com>
2020-03-06 19:05:56 +0100

    Added minLength rule

---
+++ src/Rule/MinLength.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Morebec\Validator\Rule;
+
+
+use Morebec\Validator\ValidationRuleInterface;
+
+class MinLength implements ValidationRuleInterface
+{
+    /**
+     * @var int
+     */
+    private $minLength;
+
+    /**
+     * @var string|null
+     */
+    private $message;
+
+    /**
+     * MinLength constructor.
+     * @param int $minLength
+     * @param string|null $message
+     */
+    public function __construct(
+        int $minLength,
+        ?string $message = null
+    )
+    {
+        $this->minLength = $minLength;
+        $this->message = $message;
+    }
+
+    /**
+     * Validates a value according to this rule and returns if it is valid or not
+     * @param mixed $v
+     * @return bool true if valid, otherwise false
+     */
+    public function validate($v): bool
+    {
+        return strlen($v)>=$this->minLength;
+    }
+
+    /**
+     * Returns the message to be used in case the validation did not pass
+     * @param mixed $v the value that did not pass the validation
+     * @return string
+     */
+    public function getMessage($v): string
+    {
+        return $this->message?:"'${$v}' is supposed to be at least ".$this->minLength." characters long.";
+    }
+}
---
+++ tests/Rule/MinLengthTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Morebec\Validator\Rule;
+
+
+use Morebec\Validator\Rule\MinLength;
+use PHPUnit\Framework\TestCase;
+
+class MinLengthTest extends TestCase
+{
+    public function testValidate(){
+        $ruleFirst = new MinLength(4);
+        $ruleSecond = new MinLength(4,"Custom message");
+
+        $this->assertTrue($ruleFirst->validate("test string"));
+        $this->assertFalse($ruleFirst->validate("tes"));
+        $this->assertEquals("Custom message",$ruleSecond->getMessage("test"));
+    }
+}
