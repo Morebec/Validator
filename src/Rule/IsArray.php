dbe43cfbbe8120e468f46981648856c28eabf091
edcaracas <ed.caracas@gmail.com>
2020-03-06 03:45:57 -0600

    Closes #22 - Add Message parameter to isArray (PR#16)
    Co-authored-by: hellReuter_ <hellreuter@gmail.com>

--- src/Rule/IsArray.php
+++ src/Rule/IsArray.php
@@ -3,6 +3,7 @@
 
 namespace Morebec\Validator\Rule;
 
+
 use Morebec\Validator\ValidationRuleInterface;
 
 /**
@@ -10,6 +11,19 @@ use Morebec\Validator\ValidationRuleInterface;
  */
 class IsArray implements ValidationRuleInterface
 {
+    /**
+     * @var string|null
+     */
+    private $message;
+
+    /**
+     * IsArray constructor.
+     * @param string|null $message
+     */
+    public function __construct(?string $message = null)
+    {
+        $this->message = $message;
+    }
     /**
      * @inheritDoc
      */
@@ -23,6 +37,6 @@ class IsArray implements ValidationRuleInterface
      */
     public function getMessage($v): string
     {
-        return "The value '{$v}' was expected to be an array";
+        return $this->message ?: "The value '{$v}' was expected to be an array";
     }
 }
